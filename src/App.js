9f9f7e0086e8f5396e646688534b6da61cac1181
hiro08gh <hiro08gh@gmail.com>
2019-08-23 23:33:06 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    title: '',
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/posts`).then(res => {
+      const data = res.data;
+
+      this.setState({
+        posts: data,
+      });
+    });
+  }
+
+  handleChange = e => {
+    const {name, value} = e.target;
+    this.setState({[name]: value});
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const {title} = this.state;
+
+    const data = {
+      title: title,
+    };
+
+    axios.post(`http://localhost:3001/posts`, data).then(() => {
+      console.log('success');
+    });
+  };
+
+  handleDelte = id => {
+    axios.delete(`http://localhost:3001/posts/` + id).then(() => {
+      console.log('success');
+    });
+  };
+
+  render() {
+    const {title, posts} = this.state;
+    return (
+      <div className="App">
+        App
+        <div>
+          {posts.map(post => (
+            <React.Fragment key={post.id}>
+              {post.title}
+              <button
+                onClick={() => {
+                  this.handleDelte(post.id);
+                }}>
+                削除
+              </button>
+            </React.Fragment>
+          ))}
+        </div>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>追加</button>
+      </div>
+    );
+  }
 }
-
-export default App;
